9bdbbdae9572918e0b5cd373963b8afd1bdc38bb
MAYANK-TRIPATH <mr.mayanktripathi2001@gmail.com>
2024-06-06 23:05:30 +0530

    Express-test using supertest

---
+++ express-test/src/tests/index.test.ts
@@ -0,0 +1,15 @@
+import { describe, it, expect} from '@jest/globals';
+import request from "supertest";
+import {app} from "../index";
+
+describe("POST /sum", () => {
+    it("should return the sum ", async () => {
+        const res = await request(app).post("/sum").send({
+            a: 1,
+            b: 2
+        });
+
+        expect(res.statusCode).toBe(200);
+        expect(res.body.answer).toBe(3);
+    });
+})
